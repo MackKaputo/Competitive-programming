34fc8a722a9c255ad03fa95e9ce950a058d47624
MackKaputo <mackkaputo@gmail.com>
2021-11-11 23:19:17 +0200

    fibo memo

--- Dynamic_programming/fibonacci.js
+++ Dynamic_programming/fibonacci.js
@@ -1,9 +1,16 @@
-function fib(n){
+// memoization 
+// js object, keys will be arg to fn, value will be the return value
+
+function fib(n, memo = {}){
+    if ( n in memo) {
+        return memo[n]
+    }
     if (n <= 2) {
         return 1
     }
-
-    return fib(n - 1) + fib (n - 2)
+    memo[n] = fib(n - 1) + fib (n - 2)
+    
+    return memo[n]
 }
 
 // console.log(fib(5))
