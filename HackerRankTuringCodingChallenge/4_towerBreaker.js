30394e245eb35bad0cba7dc5a2b1387fdef29529
MackKaputo <mackkaputo@gmail.com>
2021-11-16 15:37:41 +0200

    back to HackerRank week2 @towerBreaker

---
+++ HackerRankTuringCodingChallenge/4_towerBreaker.js
@@ -0,0 +1,4 @@
+
+function towerBreaker(n, m) {
+    return m == 1 || n%2 == 0? 2: 1
+}
--- codility/2_CyclicRotation.js
+++ codility/2_CyclicRotation.js
@@ -7,13 +7,14 @@ function solution(A, K) {
     }
     
     for(let i = 0; i < arrayLength; i++) {
-        let newPosition = (i+K) % arrayLength //catch index beyond array length
+        let newPosition = (i + K) % arrayLength //catch index beyond array length
         
         newArray[newPosition] = A[i];
     }
     
-    return newArray;
+    return newArray; //* return newArray.join('')  FOR STRINGS
 
 }
 
 console.log(solution([3, 8, 9, 7, 6], 3))
+console.log(solution('kop', 1).join(''))  //equivalent for string
