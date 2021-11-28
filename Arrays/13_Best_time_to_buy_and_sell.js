848cd28e8ab08bd06b4cfa8b4ee1bd0a333a8c78
MackKaputo <mackkaputo@gmail.com>
2021-11-28 03:44:06 +0200

    Completion

--- Arrays/13_Best_time_to_buy_and_sell.js
+++ Arrays/13_Best_time_to_buy_and_sell.js
@@ -66,4 +66,4 @@ console.log(maxProfit([7, 6, 4, 3, 1])) // 0
 // Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6),
 // profit = 6-1 = 5.
 // Note that buying on day 2 and selling on day 1 is not allowed
-// because you must buy before you sell.
+// because you must buy before you sell.l
